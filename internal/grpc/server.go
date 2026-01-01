d4a93af84ed8314103ee6f7a846de0905a93f580
lu <lucaswoo583@gmail.com>
2025-12-31 23:43:44 -0700

    parseGetPuzzleRequest

--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -2,11 +2,13 @@ package puzzlegrpc
 
 import (
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
-	"github.com/lucas-woo/chess-clone-v2/internal/puzzlestore"
-	"github.com/google/uuid"
-	"context"
+	"github.com/lucas-woo/chess-clone-v2/internal/puzzlestore";
+	"github.com/google/uuid";
+	"context";
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"errors";
+	"strconv"
 )
 
 type Server struct {
@@ -23,10 +25,20 @@ type PuzzleStorage interface {
 
 
 func (s *Server) GetPuzzle(_ context.Context,rq *puzzlesv1.GetPuzzleRequest) (*puzzlesv1.GetPuzzleResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetPuzzle not implemented")
+
+	v, err := parseGetPuzzleRequest(rq);
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	puzzleArr := s.store.GetPuzzles(v);
+
+	return convertPuzzleArrToGetPuzzleResponse(puzzleArr), nil
 }
 
 func (s *Server) CreatePuzzle(_ context.Context, rq *puzzlesv1.CreatePuzzleRequest) (*puzzlesv1.CreatePuzzleResponse, error) {
+
+
 	return nil, status.Errorf(codes.Unimplemented, "method CreatePuzzle not implemented")
 }
 
@@ -36,4 +48,53 @@ func (s *Server) GetPuzzleById(_ context.Context, rq *puzzlesv1.GetPuzzleByIdReq
 
 func (s *Server) DeletePuzzleById(_ context.Context, rq *puzzlesv1.DeletePuzzleByIdRequest) (*puzzlesv1.DeletePuzzleByIdResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeletePuzzleById not implemented")
+}
+
+func parseGetPuzzleRequest (rq *puzzlesv1.GetPuzzleRequest) (int32, error) {
+
+	var errs error
+	val64, err := strconv.Atoi(rq.Level)
+
+	if err != nil {
+		errors.Join(err, errors.New("Couldn't parse GetPuzzleRequest"))
+		return 0, errs
+	}
+	if val64 < 1 {
+		errors.Join(err, errors.New("Puzzel level should be greater than 0 GetPuzzleRequest"))
+		return 0, errs		
+	}
+
+	return int32(val64), errs
+}
+
+func convertPuzzleArrToGetPuzzleResponse(puzzleArr []*puzzlestore.Puzzle) *puzzlesv1.GetPuzzleResponse {
+	var returnArray []*puzzlesv1.GetPuzzleResponse_Puzzle = make([]*puzzlesv1.GetPuzzleResponse_Puzzle, 0);
+
+	for _, v := range puzzleArr {
+
+		var gameStateArr []*puzzlesv1.GetPuzzleResponse_Puzzle_PositionSchema = make([]*puzzlesv1.GetPuzzleResponse_Puzzle_PositionSchema, 0);
+
+		for _, gs := range v.GameState {
+			gameStateArr = append(gameStateArr, &puzzlesv1.GetPuzzleResponse_Puzzle_PositionSchema{
+				Piece: gs.Piece,
+				Placement: gs.Placement,
+			})
+		}
+
+		returnArray = append(returnArray, &puzzlesv1.GetPuzzleResponse_Puzzle{
+			Id: v.ID.String(),
+			GameState: gameStateArr,
+			PlayerSide: v.PlayerSide,
+			Moves: v.Moves,
+			Level: v.Level,
+		})
+	}
+
+	return &puzzlesv1.GetPuzzleResponse{
+		Puzzles: returnArray,
+	}
+}
+
+func parseCreatePuzzleRequest (rq *puzzlesv1.CreatePuzzleRequest) (*puzzlestore.Puzzle, error) {
+	return nil, nil
 }

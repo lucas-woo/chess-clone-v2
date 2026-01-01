5217d06327634a704470665c4f870a337b1cb5e8
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-01 16:18:19 -0700

    temp client server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051" ,grpc.WithTransportCredentials(insecure.NewCredentials()));
+	if err != nil {
+		log.Fatalf("error with client %v", err);
+	}
+
+	client := puzzlesv1.NewPuzzlesServiceClient(conn);
+
+	ctx := context.Background();
+
+	res, err := client.GetPuzzleById(ctx, &puzzlesv1.GetPuzzleByIdRequest{
+
+	})
+	if err != nil {
+		log.Fatalf("error with req %v", err);
+	}
+
+	fmt.Println(res)
+}

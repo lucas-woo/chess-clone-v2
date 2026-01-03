d6a661e97f147e5c9fa08c0499dade1c56ed0f52
lu <lucaswoo583@gmail.com>
2026-01-03 16:50:27 -0700

    connecting redis client

--- internal/app/rest_api/server/server.go
+++ internal/app/rest_api/server/server.go
@@ -2,12 +2,19 @@ package server
 
 import (
 	"context"
+	"errors"
 
-	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/server/router"
 	"github.com/gin-gonic/gin"
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/middlewares"
+	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/server/router"
 	create_puzzle_client "github.com/lucas-woo/chess-clone-v2/internal/grpc/puzzles/create"
+	"github.com/redis/go-redis/v9"
 );
+
+
+var (
+	redisClient *redis.Client
+)
 
 func CreateServer() *gin.Engine {
 
@@ -26,6 +33,14 @@ func CreateServer() *gin.Engine {
 	return newServer;
 }
 
-func ConnectRedis() {
-	
+func ConnectRedis() error {
+	redisClient = redis.NewClient(&redis.Options{
+        Addr: "localhost:6379",
+        Password: "", 
+        DB: 0,                
+	})
+	if redisClient == nil {
+		return errors.New("error connecting to redis client")
+	}
+	return nil
 }

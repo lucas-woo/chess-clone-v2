9bfa3da588d3da7d335ff602da1c658f17dd2d2c
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-03 16:50:27 -0700

    connecting redis client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"log"
-
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/server"
-	"github.com/redis/go-redis/v9"
 )
 
-var (
-	redisInstance *redis.Client
-)
 
 func main() {
-	server := server.CreateServer()
-	err := server.Run(":3000");
-	if err != nil {
+	
+	if err := server.ConnectRedis(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	httpServer := server.CreateServer()
+	if err := httpServer.Run(":3000"); err != nil {
 		log.Fatalf("error running client %v",err)
 	}
 }

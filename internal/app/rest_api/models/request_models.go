f7e47103264c156c170bc2d228beee8da5000ce0
lu <lucaswoo583@gmail.com>
2026-01-04 16:37:26 -0700

    request models

--- internal/app/rest_api/models/request_models.go
+++ internal/app/rest_api/models/request_models.go
@@ -1,6 +1,8 @@
 package models
 
 type CreatePuzzleRequest struct {
-	PlayerSide string `json:"session_id" binding:"required"`
-	
+	PlayerSide string `json:"playerSide" binding:"required"`
+	Level int32 `json:"level" binding:"required"`
+	GameState [][]string `json:"gameState" binding:"required"`
+	Moves []string `json:"moves" binding:"required"`
 }

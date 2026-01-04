2360b4f5da4a500cd4768af6c2830808018d07c6
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-04 02:11:14 -0700

    auth middlewares

--- internal/app/rest_api/config/redis.go
+++ internal/app/rest_api/config/redis.go
@@ -5,7 +5,12 @@ import "github.com/redis/go-redis/v9"
 var (
 	RedisClient *redis.Client
 	RedisSessionKey string = "user_session"
+	CookieSessionIDString = "session_id"
 )
+
+type UserSession struct {
+	SessionID string `json:"session_id" binding:"required"`
+}
 
 func InitRedisClient(rdb *redis.Client) {
 	RedisClient = rdb
--- internal/app/rest_api/middlewares/auth.go
+++ internal/app/rest_api/middlewares/auth.go
@@ -1,28 +1,69 @@
 package middlewares
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/config"
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/repositories"
 )
 
-func IsAlreadtLoggedIn() gin.HandlerFunc {
+func IsAlreadyLoggedIn() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		isValid, err := repositories.ValidateSessionID(ctx.Request.Context(), "");
+
+		var userSessionID config.UserSession;
+
+		sessionId, err := ctx.Cookie(config.CookieSessionIDString);
 
 		if err != nil {
+			ctx.Next()
+			return 
+		}
+		err = json.Unmarshal([]byte(sessionId), &userSessionID)
+		if err != nil {
+			ctx.Next()
+			return 			
+		}
+		isValid, err := repositories.ValidateSessionID(ctx.Request.Context(), userSessionID.SessionID);
+		if err != nil {
 			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
 		}
 		if isValid {
 			ctx.AbortWithStatus(http.StatusContinue)
+			return
 		}
-
+		ctx.Next()
 	}
 }
 
 func ProtectedRoute() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		
+		var userSessionID config.UserSession;
+
+		sessionId, err := ctx.Cookie(config.CookieSessionIDString);
+
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return 
+		}
+		err = json.Unmarshal([]byte(sessionId), &userSessionID)
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return 
+		}
+
+		isValid, err := repositories.ValidateSessionID(ctx.Request.Context(), userSessionID.SessionID);
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		if !isValid {
+			ctx.AbortWithStatus(http.StatusBadRequest)
+			return 
+		}
+		ctx.Next()
 	}
 }

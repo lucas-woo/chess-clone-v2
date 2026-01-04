441220d8cc0226c75743a19e2a5e894a09bed1e2
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-04 01:48:14 -0700

    authentication middleware

--- internal/app/rest_api/config/redis.go
+++ internal/app/rest_api/config/redis.go
@@ -4,6 +4,7 @@ import "github.com/redis/go-redis/v9"
 
 var (
 	RedisClient *redis.Client
+	RedisSessionKey string = "user_session"
 )
 
 func InitRedisClient(rdb *redis.Client) {
--- internal/app/rest_api/server/router/router.go
+++ internal/app/rest_api/server/router/router.go
@@ -1,14 +1,13 @@
 package router
 
 import (
-	"context"
 
 	"github.com/gin-gonic/gin"
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
 	
 )
 
-func InitializeRouter(ctx context.Context, router *gin.Engine, grpcClient puzzlesv1.PuzzlesServiceClient) {
+func InitializeRouter(router *gin.Engine, grpcClient puzzlesv1.PuzzlesServiceClient) {
 
 	InitializeAuthRoutes(router);
 }

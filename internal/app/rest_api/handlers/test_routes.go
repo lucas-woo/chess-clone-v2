6f52bff8f259a3e8c97c6e27a497bdd8e906e1cd
lu <lucaswoo583@gmail.com>
2026-01-04 17:24:02 -0700

    extract response

--- internal/app/rest_api/handlers/test_routes.go
+++ internal/app/rest_api/handlers/test_routes.go
@@ -1,14 +1,29 @@
 package handlers
 
 import (
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
+	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/models"
 )
 
 func CreatePuzzleRequest(puzzleGrpcCient puzzlesv1.PuzzlesServiceClient) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var puzzle *puzzlesv1.CreatePuzzleRequest;
+		temp, exists := ctx.Get(models.CreatedPuzzleKey)
+		if !exists {
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		puzzle = temp.(*puzzlesv1.CreatePuzzleRequest)
+		res, err := puzzleGrpcCient.CreatePuzzle(ctx.Request.Context(), puzzle)
 
-
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return			
+		}
+		var response models.CreatePuzzleResponse = models.CreatePuzzleResponse{Id: res.Id}
+		ctx.JSON(http.StatusCreated, response)
 	}
 }
--- internal/app/rest_api/server/router/test_routes.go
+++ internal/app/rest_api/server/router/test_routes.go
@@ -4,8 +4,9 @@ import (
 	"github.com/gin-gonic/gin"
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/handlers"
+	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/middlewares"
 )
 
 func InitializeTestRoutes(router *gin.Engine, puzzleGRPCclient puzzlesv1.PuzzlesServiceClient) {
-	router.POST("/newPuzzle", handlers.CreatePuzzleRequest(puzzleGRPCclient))
+	router.POST("/newPuzzle", middlewares.ExtractPuzzle(), handlers.CreatePuzzleRequest(puzzleGRPCclient))
 }

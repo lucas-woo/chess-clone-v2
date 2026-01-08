62375ce357336bbf66cf07bbc4dbce736ebec10c
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-08 00:10:28 -0700

    registering auth server

--- cmd/servers/auth/main.go
+++ cmd/servers/auth/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 
-	"github.com/lucas-woo/chess-clone-v2/pkg/db/mongodb"
+	authv1 "github.com/lucas-woo/chess-clone-v2/api/authentication/v1"
+	"github.com/lucas-woo/chess-clone-v2/internal/grpc/repositories"
+	authenticationgrpc "github.com/lucas-woo/chess-clone-v2/internal/grpc/servers/authentication"
 	"github.com/lucas-woo/chess-clone-v2/pkg/config"
+	"github.com/lucas-woo/chess-clone-v2/pkg/db/mongodb"
+	redisclient "github.com/lucas-woo/chess-clone-v2/pkg/redis"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main() {
@@ -14,6 +22,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = mongodb.ConnectMongo()
 
 	if err != nil {
@@ -21,4 +30,28 @@ func main() {
 	}
 	fmt.Println("connected mongo")
 
+	redisClient, err := redisclient.ConnectRedis()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lis, err := net.Listen("tcp", ":50051")
+
+	if err != nil {
+		log.Fatalf("err %v", err);
+	}
+
+	grpcServer := grpc.NewServer();
+
+	loginCollection := repositories.GetUserLoginCollection()
+	authv1.RegisterAuthenticationServiceServer(grpcServer, authenticationgrpc.NewServer(redisClient, loginCollection))
+
+	healthServer := health.NewServer()
+	healthv1.RegisterHealthServer(grpcServer, healthServer);
+	fmt.Println("puzzle server running")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("err in starting grpc server: %v",err)
+	}	
 }

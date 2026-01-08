779e974b355e8bdc48bdc147272ba80e6ec6cfe2
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-07 21:36:38 -0700

    parsing signup request

--- internal/grpc/servers/authentication/server.go
+++ internal/grpc/servers/authentication/server.go
@@ -2,8 +2,11 @@ package authenticationgrpc
 
 import (
 	"context"
+	"errors"
 
+	"github.com/google/uuid"
 	authv1 "github.com/lucas-woo/chess-clone-v2/api/authentication/v1"
+	"github.com/lucas-woo/chess-clone-v2/internal/grpc/models"
 	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
@@ -11,11 +14,10 @@ import (
 type Server struct {
 	redisClient *redis.Client
 	userLoginCollection *mongo.Collection
-	userProfileCollection *mongo.Collection
 	authv1.UnimplementedAuthenticationServiceServer; 
 }
 
-func (s *Server) SignUpUser(context.Context, *authv1.SignUpUserRequest) (*authv1.SignUpUserResponse, error) {
+func (s *Server) SignUpUser(ctx context.Context, signupRequest *authv1.SignUpUserRequest) (*authv1.SignUpUserResponse, error) {
 	
 	return nil, nil
 }
@@ -29,10 +31,39 @@ func (s *Server) VerifyUsername(context.Context, *authv1.VerifyUsernameRequest) 
 	return nil, nil
 }
 
-func NewServer(redisClient *redis.Client, userLoginCollection *mongo.Collection, userProfileCollection *mongo.Collection) (*Server) {
+
+func parseSignUpUserRequest(signupRequest *authv1.SignUpUserRequest) (*models.UserLogin, error) {
+	var err error
+	//gotta add validation later, this isn't good
+	if len(signupRequest.Email) == 0 {
+		newErr := errors.New("invalid email")
+		err = errors.Join(err, newErr)
+	}
+	if len(signupRequest.Username) == 0 {
+		newErr := errors.New("invalid username")
+		err = errors.Join(err, newErr)		
+	}
+	if len(signupRequest.HashedPassword) == 0 {
+		newErr := errors.New("invalid password")
+		err = errors.Join(err, newErr)
+	}
+	if err != nil {
+		return nil, err
+	}
+	//gotta also double check if the username isn't taken, can't trust rest api
+ 	return &models.UserLogin{
+		Username: signupRequest.Username,
+		Hash: signupRequest.HashedPassword,
+		Email: signupRequest.Email,
+		UserID: uuid.New(),
+	}, nil
+}
+
+
+
+func NewServer(redisClient *redis.Client, userLoginCollection *mongo.Collection) (*Server) {
 	return &Server{
 		redisClient: redisClient,
 		userLoginCollection: userLoginCollection,
-		userProfileCollection: userProfileCollection,
 	}
 }

e25100f7ee1988f3e10f5a45474de624ce285355
lu <lucaswoo583@gmail.com>
2026-01-14 23:44:31 -0700

    auth client func

--- internal/grpc/servers/authentication/create/create_auth_grpc_client.go
+++ internal/grpc/servers/authentication/create/create_auth_grpc_client.go
@@ -1,8 +1,30 @@
 package create
 
-// import "google.golang.org/grpc"
+import (
+	"log"
+	"os"
+
+	authv1 "github.com/lucas-woo/chess-clone-v2/api/authentication/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 
-// func CreateGRPCAuthClient() {
-// 	conn, err := grpc.NewClient("localhost:50052")
-// }
+func CreateGRPCAuthClient() authv1.AuthenticationServiceClient {
+	
+	port, found := os.LookupEnv("AUTH_CLIENT_PORT")
+
+	if !found {
+		log.Fatal("error with auth port env");
+	}
+
+	conn, err := grpc.NewClient("localhost:" + port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	client := authv1.NewAuthenticationServiceClient(conn)
+
+	return client
+}

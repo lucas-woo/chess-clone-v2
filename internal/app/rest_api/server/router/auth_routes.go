13aa5c4ea13be55c9f1a5aa6c93c2db6027b9e60
lu <lucaswoo583@gmail.com>
2026-01-15 18:10:28 -0700

    logout handler

--- internal/app/rest_api/server/router/auth_routes.go
+++ internal/app/rest_api/server/router/auth_routes.go
@@ -10,7 +10,5 @@ import (
 func InitializeAuthRoutes(r *gin.Engine, authClient authv1.AuthenticationServiceClient) {
 	r.POST("/signup", middlewares.IsAlreadyLoggedIn(), middlewares.ValidateSignUp(), handlers.SignUp(authClient))
 	r.POST("/login", middlewares.IsAlreadyLoggedIn(), middlewares.ValidateSignUp(), handlers.Login(authClient))
-	r.POST("/logout", )
-	r.GET("verify-username");
-	r.PUT("update-username", middlewares.ProtectedRoute());
+	r.POST("/logout", middlewares.ProtectedRoute(), handlers.Logout(authClient))
 }

6b57706bc484e80ea5f03af39b1b2c6be9fea79e
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-15 21:52:43 -0700

    rework... oops

--- internal/app/rest_api/models/auth_model.go
+++ internal/app/rest_api/models/auth_model.go
@@ -4,7 +4,7 @@ type UserLogin struct {
 
 }
 
-type UserSignUp struct {
+type UserSignUpData struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 	Email string `json:"email" binding:"required"`

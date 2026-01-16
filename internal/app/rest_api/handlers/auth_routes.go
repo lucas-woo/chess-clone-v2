cfc0f5b557b38525bef4e091af6ee9eb5005fb8d
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-15 17:03:16 -0700

    signup handler

--- internal/app/rest_api/handlers/auth_routes.go
+++ internal/app/rest_api/handlers/auth_routes.go
@@ -6,14 +6,45 @@ import (
 	"github.com/gin-gonic/gin"
 	authv1 "github.com/lucas-woo/chess-clone-v2/api/authentication/v1"
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/config"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func SignUp(authClient authv1.AuthenticationServiceClient) gin.HandlerFunc {
 	return func (c *gin.Context) {
-		
-		newUser, err := authClient.SignUpUser(c.Request.Context(), &authv1.SignUpUserRequest{
 
+		username, exists := c.Get("username")
+		usernameConv, ok := username.(string)
+		if !exists || !ok {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return			
+		}
+
+		password, exists := c.Get("password")
+		passwordConv, ok := password.(string)
+		if !exists || !ok{
+			c.AbortWithStatus(http.StatusBadRequest)
+			return			
+		}		
+
+		email, exists := c.Get("email")
+		emailConv, ok := email.(string)
+		if !exists || !ok{
+			c.AbortWithStatus(http.StatusBadRequest)
+			return			
+		}		
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(passwordConv), bcrypt.DefaultCost)
+		if err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return				
+		}
+
+		newUser, err := authClient.SignUpUser(c.Request.Context(), &authv1.SignUpUserRequest{
+			Username: usernameConv,
+			Email: emailConv,
+			HashedPassword: string(hashedPassword),
 		})
+
 		if err != nil || newUser.SignupError != authv1.SignUpUserResponse_SIGN_UP_ERROR_UNSPECIFIED {
 			c.AbortWithStatus(http.StatusBadRequest)
 			return
@@ -21,5 +52,6 @@ func SignUp(authClient authv1.AuthenticationServiceClient) gin.HandlerFunc {
 
 		c.SetCookie(config.CookieSessionIDString, newUser.SessionId, config.CookieSessionMaxAge, config.CookieSessionPath, config.CookieSessionDomain, config.CookieSessionSecure, config.CookieSessionHttpOnly)
 
+		c.JSON(http.StatusCreated, "created")
 	}
 }

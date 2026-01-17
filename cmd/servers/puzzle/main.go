fd5e01e93df33f03741a173cfb2c8df812b8157f
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-16 23:01:23 -0700

    puzzle client testing

--- cmd/servers/puzzle/main.go
+++ cmd/servers/puzzle/main.go
@@ -8,12 +8,26 @@ import (
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
 	"github.com/lucas-woo/chess-clone-v2/internal/grpc/repositories"
 	puzzlegrpcserver "github.com/lucas-woo/chess-clone-v2/internal/grpc/servers/puzzles"
+	"github.com/lucas-woo/chess-clone-v2/pkg/config"
+	"github.com/lucas-woo/chess-clone-v2/pkg/db/mongodb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func main () {
+
+	err := config.InitializeEnv();
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = mongodb.ConnectMongo()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected mongo")
+
 	lis, err := net.Listen("tcp", ":50051")
 
 	if err != nil {

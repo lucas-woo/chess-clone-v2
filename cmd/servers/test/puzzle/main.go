d51cb301ac287c9412fc43dbaf9dc9a245c583e0
lu <lucaswoo583@gmail.com>
2026-01-16 23:11:19 -0700

    wrong port fix

--- cmd/servers/test/puzzle/main.go
+++ cmd/servers/test/puzzle/main.go
@@ -1,8 +1,27 @@
 package main
 
-import client "github.com/lucas-woo/chess-clone-v2/internal/grpc/servers/puzzles/create"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
+	client "github.com/lucas-woo/chess-clone-v2/internal/grpc/servers/puzzles/create"
+	"github.com/lucas-woo/chess-clone-v2/pkg/config"
+)
 
 func main() {
-	
-	client.CreateGRPCPuzzleClient()
+	err := config.InitializeEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	puzzleClient := client.CreateGRPCPuzzleClient()
+	ctx := context.Background()
+	res, err := puzzleClient.GetPuzzle(ctx, &puzzlesv1.GetPuzzleRequest{
+		Level: 2,
+	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(res)
 }

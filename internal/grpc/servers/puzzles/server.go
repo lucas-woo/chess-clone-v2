71f278a47e749948a07b465d5f16e78b4bb2b71e
lu <lucaswoo583@gmail.com>
2026-01-16 17:55:33 -0700

    aggregate puzzle

--- internal/grpc/servers/puzzles/server.go
+++ internal/grpc/servers/puzzles/server.go
@@ -2,18 +2,27 @@ package puzzlegrpc
 
 import (
 	"context"
-
+	"errors"
 
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
+	"go.mongodb.org/mongo-driver/v2/mongo"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Server struct {
 	puzzlesv1.UnimplementedPuzzlesServiceServer
+	puzzleCollection *mongo.Collection
 }
 
-func (s *Server) GetPuzzle(context.Context, *puzzlesv1.GetPuzzleRequest) (*puzzlesv1.GetPuzzleResponse, error) {
+func (s *Server) GetPuzzle(ctx context.Context, req *puzzlesv1.GetPuzzleRequest) (*puzzlesv1.GetPuzzleResponse, error) {
+
+	level, err := parseGetPuzzleRequest(req)
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+	s.puzzleCollection.Find(ctx, )
+
 	return nil, status.Errorf(codes.Unimplemented, "method GetPuzzle not implemented")
 }
 func (s *Server) CreatePuzzle(context.Context, *puzzlesv1.CreatePuzzleRequest) (*puzzlesv1.CreatePuzzleResponse, error) {
@@ -26,12 +35,15 @@ func (s *Server) DeletePuzzleById(context.Context, *puzzlesv1.DeletePuzzleByIdRe
 	return nil, status.Errorf(codes.Unimplemented, "method DeletePuzzleById not implemented")
 }
 
-func parseGetPuzzleRequest (rq *puzzlesv1.GetPuzzleRequest) (int32, error) {
-	return 0, nil
+func parseGetPuzzleRequest (getRequest *puzzlesv1.GetPuzzleRequest) (int32, error) {
+	if getRequest.Level < 1 {
+		return 0, errors.New("invalid request");
+	}
+	return getRequest.Level, nil
 }
 
-func NewServer () *Server {
+func NewServer (puzzleCollection *mongo.Collection) *Server {
 	return &Server{
-
+		puzzleCollection: puzzleCollection,
 	};
 }

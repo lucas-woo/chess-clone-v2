f026f5b5cb87b4a9554c989e86d1ce4103d102a2
lu <241841693+lucas-woo@users.noreply.github.com>
2026-01-16 17:38:53 -0700

    puzzle schemas

--- internal/grpc/models/puzzles.go
+++ internal/grpc/models/puzzles.go
@@ -1,1 +1,17 @@
 package models
+
+import "go.mongodb.org/mongo-driver/v2/bson"
+
+
+type PuzzleSchema struct {
+	ID bson.ObjectID `bson:"_id,omitempty"`
+	GameState []PuzzlePositionSchema `bson:"gameState,omitempty"`
+	PlayerSide string `bson:"playerSide,omitempty"`
+	Moves []string `bson:"moves,omitempty"`
+	Level int32 `bson:"level,omitempty"`
+}
+
+type PuzzlePositionSchema struct {
+	Piece string`bson:"piece,omitempty"`
+	Placement string `bson:"placement,omitempty"`
+}

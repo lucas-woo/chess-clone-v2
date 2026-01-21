39f000b572b394251f8c697da383a3c5c9b5a2a1
lu <lucaswoo583@gmail.com>
2026-01-20 20:57:15 -0700

    get puzzle route

--- internal/app/rest_api/server/router/puzzle_routes.go
+++ internal/app/rest_api/server/router/puzzle_routes.go
@@ -3,11 +3,13 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
+	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/handlers"
+	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/middlewares"
 )
 
 func InitializePuzzleRoutes(router *gin.Engine, puzzleClient puzzlesv1.PuzzlesServiceClient) {
-	router.GET("/puzzles/get")
-	router.POST("/puzzles/create")
-	router.GET("/puzzles/get-by-id")
+	router.GET("/puzzles/get", middlewares.ProtectedRoute(), handlers.CreatePuzzleRequest(puzzleClient))
+	router.POST("/puzzles/create")//admin
+	router.GET("/puzzles/get-by-id" )// admin
 	router.DELETE("/puzzles/delete")
 }

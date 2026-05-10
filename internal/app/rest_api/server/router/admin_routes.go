65fa95001b9c9cf0c47c7005810ebaee6728235d
lu <lucaswoo583@gmail.com>
2026-05-09 20:14:09 -0600

    rework puzzle routes

--- internal/app/rest_api/server/router/admin_routes.go
+++ internal/app/rest_api/server/router/admin_routes.go
@@ -1,5 +1,14 @@
 package router
 
-func initializeAdminRoutes () {
-	
+import (
+	"github.com/gin-gonic/gin"
+	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
+)
+
+func initializeAdminRoutes (router *gin.Engine, puzzleClient puzzlesv1.PuzzlesServiceClient) {
+
+
+	router.POST("/puzzles/create", )//admin
+	router.GET("/puzzles/get-by-id" )// admin
+	router.DELETE("/puzzles/delete")//admin
 }

fbed73aa7345a2ff8a44d74d6c6f7328a8c5b72f
lu <241841693+lucas-woo@users.noreply.github.com>
2026-05-09 20:08:39 -0600

    remove public

--- internal/app/rest_api/server/router/auth_routes.go
+++ internal/app/rest_api/server/router/auth_routes.go
@@ -7,7 +7,7 @@ import (
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/middlewares"
 )
 
-func InitializeAuthRoutes(r *gin.Engine, authClient authv1.AuthenticationServiceClient) {
+func initializeAuthRoutes(r *gin.Engine, authClient authv1.AuthenticationServiceClient) {
 	r.POST("/auth/signup", middlewares.IsAlreadyLoggedIn(), middlewares.ValidateSignUp(), handlers.SignUp(authClient))
 	r.POST("/auth/login", middlewares.IsAlreadyLoggedIn(), middlewares.ValidateLogin(), handlers.Login(authClient))
 	r.POST("/auth/logout", middlewares.ProtectedRoute(), handlers.Logout(authClient))
--- internal/app/rest_api/server/router/test_routes.go
+++ internal/app/rest_api/server/router/test_routes.go
@@ -7,6 +7,6 @@ import (
 	"github.com/lucas-woo/chess-clone-v2/internal/app/rest_api/middlewares"
 )
 
-func InitializeTestRoutes(router *gin.Engine, puzzleGRPCclient puzzlesv1.PuzzlesServiceClient) {
+func initializeTestRoutes(router *gin.Engine, puzzleGRPCclient puzzlesv1.PuzzlesServiceClient) {
 	router.POST("/new-puzzle", middlewares.ExtractPuzzle(), handlers.CreatePuzzleRequest(puzzleGRPCclient))
 }

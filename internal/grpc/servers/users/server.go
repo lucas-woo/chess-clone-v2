b942e09fc8761815ff52e8479ef39e3cc1978f16
lu <lucaswoo583@gmail.com>
2026-05-18 17:35:35 -0600

    proto fix

--- internal/grpc/servers/users/server.go
+++ internal/grpc/servers/users/server.go
@@ -14,19 +14,32 @@ type Server struct {
 }
 
 
-func (s *Server) GetUserProfile(context.Context, *usersv1.GetUserProfileRequest) (*usersv1.GetUserProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetUserProfile not implemented")
-}
-func (s *Server) GetMyProfile(context.Context, *usersv1.GetMyProfileRequest) (*usersv1.GetMyProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetMyProfile not implemented")
-}
-func (s *Server) UpdateUserProfile(context.Context, *usersv1.UpdateUserProfileRequest) (*usersv1.UpdateUserProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateUserProfile not implemented")
-}
-func (s *Server) SaveUserScore(context.Context, *usersv1.SaveUserScoreRequest) (*usersv1.SaveUserScoreResponse, error) {
+func (s *Server) SaveUserScore(ctx context.Context, req *usersv1.SaveUserScoreRequest) (*usersv1.SaveUserScoreResponse, error) {
+
+	_, err := parseSaveUserScoreRequest(req);
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	//check score if score is higher than current profile 
+
 	return nil, status.Errorf(codes.Unimplemented, "method SaveUserScore not implemented")
 }
 
+func (s *Server) GetUserProfile(ctx context.Context, req *usersv1.GetUserProfileRequest) (*usersv1.GetUserProfileResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetUserProfile not implemented")
+}
+func (s *Server) GetMyProfile(ctx context.Context, req *usersv1.GetMyProfileRequest) (*usersv1.GetMyProfileResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMyProfile not implemented")
+}
+func (s *Server) UpdateUserProfile(ctx context.Context, req *usersv1.UpdateUserProfileRequest) (*usersv1.UpdateUserProfileResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateUserProfile not implemented")
+}
+
+func parseSaveUserScoreRequest(req *usersv1.SaveUserScoreRequest) (int32, error) {
+	return req.Score, nil
+}
 
 func NewServer() *Server {
 	return &Server{

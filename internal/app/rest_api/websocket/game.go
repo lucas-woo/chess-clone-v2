4caa4622ccbd3b5bd477835bc4db5c7ad7232a01
lu <241841693+lucas-woo@users.noreply.github.com>
2026-06-02 21:55:48 -0600

    socket functionality done

--- internal/app/rest_api/websocket/game.go
+++ internal/app/rest_api/websocket/game.go
@@ -3,7 +3,7 @@ package websocket
 import (
 	"context"
 	"time"
-
+	"strconv"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	puzzlesv1 "github.com/lucas-woo/chess-clone-v2/api/puzzles/v1"
@@ -19,6 +19,7 @@ type Game struct {
 
 type GameMessage struct {
 	Message string `json:"message"`
+	Content any
 }
 
 func NewGame (conn *websocket.Conn, gamePool *GamePool, userId string) *Game {
@@ -38,28 +39,49 @@ func (g *Game) RunGame(c *gin.Context, puzzleClient puzzlesv1.PuzzlesServiceClie
 	go func() {
 		<-ctx.Done()
 
-		g.gamePool.leave <- g;
+		defer func() {
+			g.gamePool.leave <- g;
+			g.socket.Close()			
+		}()
 		
 		err := g.socket.WriteJSON(&GameMessage{
-			Message: string(g.currentLevel),
+			Message: "end",
+			Content: strconv.Itoa(g.currentLevel),
 		})
 		if err != nil {
 			g.socket.Close()
 			return 
 		}
 
-		//save in server
-
-		g.socket.Close()
-
+		newCtx := c.Request.Context()
+		profileClient.SaveUserScore(newCtx, &usersv1.SaveUserScoreRequest{
+			Score: uint32(g.currentLevel),
+			UserId: g.userId,
+		})
 	}()
 
-	err := g.socket.WriteJSON(&GameMessage{
+	err := g.socket.WriteJSON(GameMessage{
 		Message: "start",
 	})
 	if err != nil {
 		return
 	}
+
+	newCtx := c.Request.Context()
+	res, err := puzzleClient.GetPuzzle(newCtx, &puzzlesv1.GetPuzzleRequest{
+		Level: 1,
+	})
+	if err != nil {
+		return
+	}
+	err = g.socket.WriteJSON(GameMessage{
+		Message: "puzzles",
+		Content: res,
+	})
+	if err != nil {
+		return
+	}
+
 
 	for {
 		var msg GameMessage
@@ -72,9 +94,20 @@ func (g *Game) RunGame(c *gin.Context, puzzleClient puzzlesv1.PuzzlesServiceClie
 		}
 		g.currentLevel++;
 		if g.currentLevel % 5 == 0 {
-			//send req and send client
+			newCtx := c.Request.Context()
+			res, err := puzzleClient.GetPuzzle(newCtx, &puzzlesv1.GetPuzzleRequest{
+				Level: int32((g.currentLevel + 5) / 5),
+			})
+			if err != nil {
+				return
+			}
+			err = g.socket.WriteJSON(GameMessage{
+				Message: "puzzles",
+				Content: res,
+			})
+			if err != nil {
+				return
+			}
 		}
-
 	}
-
 }
